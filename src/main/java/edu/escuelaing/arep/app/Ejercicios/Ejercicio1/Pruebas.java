9e4f7f47adfe6adf266e306204daa4603456a5e5
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-02-16 14:38:44 -0500

    Creando Pruebas con etiqueta retention.

---
+++ src/main/java/edu/escuelaing/arep/app/Ejercicios/Ejercicio1/Pruebas.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arep.app.Ejercicios.Ejercicio1;
+
+public class Pruebas {
+    public void m1(){}
+
+    @Test
+    public static void m2(){
+        throw new RuntimeException("Error m2");
+    }
+
+    @Test
+    public static void m3(){}
+
+    @Test
+    public static void m4(){
+        throw new RuntimeException("Error m4");
+    }
+
+    public void m5(){}
+}
